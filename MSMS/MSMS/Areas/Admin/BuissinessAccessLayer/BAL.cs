706adbbe529f026d3922d4ff94f7ebc5bd9c67eb
pravallikavtalent <pravallika.vtalent@gmail.com>
2019-09-05 15:56:43 +0530

    repository added

---
+++ MSMS/MSMS/Areas/Admin/BuissinessAccessLayer/BAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MSMS.Areas.Admin.IRepository;
+using MSMS.Areas.Admin.Repository;
+
+
+namespace MSMS.Areas.Admin.BuissinessAccessLayer
+{
+    public class BAL
+    {
+        IReposit Iobj;
+        public BAL(Reposit _Iobj)
+        {
+            
+        }
+    }
+}
---
+++ MSMS/MSMS/Areas/Admin/Repository/Reposit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSMS.Areas.Admin.Repository
+{
+    public class Reposit
+    {
+    }
+}
